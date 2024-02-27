113ac4e774dd85ccef2040e7c3c4c39dc430e1d2
Ahmed Mahmoud <56558577+AhmedAbouelkher@users.noreply.github.com>
2024-02-27 17:17:05 +0200

    Add new concurrency features & more

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
 import os
+from concurrent.futures import ThreadPoolExecutor
 
 
 def clean_flutter_build(path: str):
-    for root, dirs, files in os.walk(path):
-        for file in files:
-            if file.__contains__(".flutter-plugins"):
-                _root = root.replace(" ", "\\ ")
-                print("[Cleaning] " + _root)
-                command = "cd " + _root + " && flutter clean"
-                os.system(command)
-                print("[DONE Cleaning]", '*' * 60)
+    with ThreadPoolExecutor(max_workers=4) as executor:
+        for root, dirs, files in os.walk(path):
+            for file in files:
+                if file.__contains__(".flutter-plugins"):
+                    _root = root.replace(" ", "\\ ")
+                    print(f"[Cleaning] {_root}")
+                    # Submit the cleaning task to the thread pool
+                    executor.submit(_clean_subfolder, _root)
 
 
-if __name__ == '__main__':
-    apps_dir = "/Users/ahmedmahmoud/AndroidStudioProjects/test_clean_utility/"
-    clean_flutter_build(apps_dir)
+def _clean_subfolder(root_path):
+    command = f"cd {root_path} && flutter clean"
+    os.system(command)
+    print(f"[DONE Cleaning] {'*' * 60}")
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Clean Flutter projects")
+    parser.add_argument(
+        "apps_dir", nargs="+", help="Paths to directories containing Flutter projects"
+    )
+    args = parser.parse_args()
+
+    for directory in args.apps_dir:
+        clean_flutter_build(directory)
